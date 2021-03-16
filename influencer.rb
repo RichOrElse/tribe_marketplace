c7cd793ecf54186f79f1a6d2d243b625177637a4
Ritchie Paul Buitre <ritchie@richorelse.com>
2021-03-16 19:30:20 +0800

    Make test case '13 VID $2370' pass. - fixed test data - replace Bundles#fetch with Bundles#by_total_posts

--- influencer.rb
+++ influencer.rb
@@ -1,11 +1,15 @@
 class Influencer < Struct.new(:bundles)
   def initialize(bundles)
-    super(bundles.extend Bundles)
+    @bundles_for = bundles.group_by(&:format_code)
+                          .each { |_, group| group.extend Bundles }
+    super
   end
 
   def charge(order)
     order.inject(Hash.new(Breakdown.new [])) do |charged, line|
-      charged[line.format_code] = Breakdown.new bundles.fetch(line)
+      charged[line.format_code] = Breakdown.new(
+        @bundles_for[line.format_code].by_total_posts(line.size)
+      )
       charged
     end
   end
@@ -26,25 +30,8 @@ Breakdown = Struct.new(:bundles) do
 end
 
 module Bundles
-  def fetch(line, found = [])
-    biggest = select(&HasFormatCode[line]).sort_by(&:size)
-
-    size = line.size
-    bundle = biggest.pop
-
-    while size > 0
-      return found unless bundle
-
-      found << bundle
-      size -= bundle.size
-
-      bundle = biggest.pop if size < bundle.size
-    end
-
-    found
-  end
-
-  HasFormatCode = proc do |line|
-    proc { |other| line.format_code == other.format_code }
+  def by_total_posts(size, length = 1)
+    repeated_combination(length)
+      .find { |batch| batch.sum(&:size) == size } or by_total_posts(size, length + 1)
   end
 end
--- spec/influencer_spec.rb
+++ spec/influencer_spec.rb
@@ -16,24 +16,23 @@ describe Influencer do
 
   describe "#charge" do
     let(:charges) { @influencer.charge([subject]) }
+    let(:breakdown) { charges[subject.format_code]}
 
     describe OrderLine[10, 'IMG'] do
-      let(:breakdown) { charges['IMG'] }
       specify "10 IMG $800" do
         expect(breakdown.total_posts).to eq 10
         expect(breakdown.total_cost).to eq 800
-        expect(breakdown.bundles).to eq [
+        expect(breakdown.bundles).to match_array [
           Bundle['IMG', 10, 800]
         ]
       end
     end
 
     describe OrderLine[15, 'FLAC'] do
-      let(:breakdown) { charges['FLAC'] }
       specify "15 FLAC $1957.50" do
         expect(breakdown.total_posts).to eq 15
         expect(breakdown.total_cost).to eq 1957.50
-        expect(breakdown.bundles).to eq [
+        expect(breakdown.bundles).to match_array [
           Bundle['FLAC', 9, 1147.50],
           Bundle['FLAC', 6, 810],
         ]
@@ -41,13 +40,12 @@ describe Influencer do
     end
 
     describe OrderLine[13, 'VID'] do
-      let(:breakdown) { charges['IMG'] }
       specify "13 VID $2370" do
         expect(breakdown.total_posts).to eq 13
         expect(breakdown.total_cost).to eq 2370
-        expect(breakdown.bundles).to eq [
-          Bundle['VID', 5, 1800],
-          Bundle['VID', 5, 1800],
+        expect(breakdown.bundles).to match_array [
+          Bundle['VID', 5, 900],
+          Bundle['VID', 5, 900],
           Bundle['VID', 3, 570],
         ]
       end
