d5e706110c2bb6b167acbf5f5013f649b5e509f1
Ritchie Paul Buitre <ritchie@richorelse.com>
2021-03-16 12:34:33 +0800

    Make test case '10 IMG $800' pass

--- influencer.rb
+++ influencer.rb
@@ -1,10 +1,27 @@
 class Influencer < Struct.new(:bundles)
   def charge(order)
-  	Hash.new(Breakdown.new)
+    order.inject(Hash.new(Breakdown.new [])) do |charged, line|
+      charged[line.format_code] = Breakdown.new bundles.grep(line)
+
+      charged
+    end
   end
 end
 
 Bundle = Struct.new(:format_code, :size, :cost)
-OrderLine = Struct.new(:quantity, :format_code)
-Breakdown = Struct.new(:total_posts, :total_cost, :bundles)
 
+OrderLine = Struct.new(:size, :format_code) do
+  def ===(other)
+    size == other.size && format_code == other.format_code
+  end
+end
+
+Breakdown = Struct.new(:bundles) do
+  def total_cost
+    bundles.sum(&:cost)
+  end
+
+  def total_posts
+    bundles.sum(&:size)
+  end
+end
