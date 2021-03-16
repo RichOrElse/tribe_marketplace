4482f479b414e026caac7d737e7e2a3a6cc7f289
Ritchie Paul Buitre <ritchie@richorelse.com>
2021-03-16 14:26:11 +0800

    Make test case: '15 FLAC .50' pass

--- influencer.rb
+++ influencer.rb
@@ -1,8 +1,11 @@
 class Influencer < Struct.new(:bundles)
+  def initialize(bundles)
+    super(bundles.extend Bundles)
+  end
+
   def charge(order)
     order.inject(Hash.new(Breakdown.new [])) do |charged, line|
-      charged[line.format_code] = Breakdown.new bundles.grep(line)
-
+      charged[line.format_code] = Breakdown.new bundles.fetch(line)
       charged
     end
   end
@@ -10,11 +13,7 @@ end
 
 Bundle = Struct.new(:format_code, :size, :cost)
 
-OrderLine = Struct.new(:size, :format_code) do
-  def ===(other)
-    size == other.size && format_code == other.format_code
-  end
-end
+OrderLine = Struct.new(:size, :format_code)
 
 Breakdown = Struct.new(:bundles) do
   def total_cost
@@ -23,5 +22,29 @@ Breakdown = Struct.new(:bundles) do
 
   def total_posts
     bundles.sum(&:size)
+  end
+end
+
+module Bundles
+  def fetch(line, found = [])
+    biggest = select(&HasFormatCode[line]).sort_by(&:size)
+
+    size = line.size
+    bundle = biggest.pop
+
+    while size > 0
+      return found unless bundle
+
+      found << bundle
+      size -= bundle.size
+
+      bundle = biggest.pop if size < bundle.size
+    end
+
+    found
+  end
+
+  HasFormatCode = proc do |line|
+    proc { |other| line.format_code == other.format_code }
   end
 end
