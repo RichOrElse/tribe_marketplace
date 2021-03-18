7d99a48274d59b5c6f28fb23db26c197c796fc99
Ritchie Paul Buitre <ritchie@richorelse.com>
2021-03-18 17:35:08 +0800

    Convert Bundle#cost to BigDecimal

--- lib/bundle.rb
+++ lib/bundle.rb
@@ -1,4 +1,10 @@
+require 'bigdecimal/util'
+
 class Bundle < Struct.new(:format_code, :size, :cost)
+  def initialize(format_code, size = 1, cost = 0.0)
+    super(format_code, size.to_i, cost.to_d)
+  end
+
   def + other
     Bundle.new format_code, size + other.size, cost + other.cost
   end
