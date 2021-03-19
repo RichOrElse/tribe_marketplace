e6ab50a0c25a79a0ccedf08ec90ef556e3cc67a7
Ritchie Paul Buitre <ritchie@richorelse.com>
2021-03-19 10:31:58 +0800

    Limit combination length within possible combinations - add minimum and maximum to QueryObject

--- lib/queries/by_size.rb
+++ lib/queries/by_size.rb
@@ -1,12 +1,20 @@
 class BySize < QueryObject
-  def combined(total, length = 1)
-    return [] if length > total
+  def combined(total, length = 1, possible = max_combinations_within(total))
+    return [] if length > possible
 
     repeated_combination(length)
-      .find { |combination| combination.sum(&:size) == total } || combined(total, length + 1)
+      .find { |combination| combination.sum(&:size) == total } || combined(total, length + 1, possible)
   end
 
   def breakdowns
     group_by(&:size).map(&Breakdown).reverse
   end
+
+  private
+
+    def max_combinations_within(accumulated_size)
+      return 0 if none?
+
+      (accumulated_size / minimum(:size).to_f).ceil
+    end
 end
