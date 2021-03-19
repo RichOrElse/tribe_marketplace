a1e53cdbd87a94730b8395dd93030b7a3bd4a2c0
Ritchie Paul Buitre <ritchie@richorelse.com>
2021-03-19 10:31:58 +0800

    Limit combination length within possible combinations

--- lib/queries/query_object.rb
+++ lib/queries/query_object.rb
@@ -4,4 +4,12 @@ class QueryObject < SimpleDelegator
   def self.to_proc
     method(:new).to_proc
   end
+
+  def minimum(key, *options)
+    map(&key).min(*options)
+  end
+
+  def maximum(key, *options)
+    map(&key).max(*options)
+  end
 end
